1a80fdddac44bd572c1068a6b1e3cccbd03e1956
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-03-29 23:58:42 +0000

    initial js code

--- TODO/todo.js
+++ TODO/todo.js
@@ -1,1 +1,33 @@
 let ul = document.getElementById('todo');
+let input = document.getElementById('taskInput');
+let button = document.getElementById('addTask');
+
+button.addEventListener('click enterkey', function() {
+    let task = input.value;
+    if (task) {
+        let li = document.createElement('li');
+        li.textContent = task;
+        ul.appendChild(li);
+        input.value = '';
+    }
+    else {        alert('Please enter a task!');
+    }           
+}
+);
+
+ul.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        ul.removeChild(event.target);
+    }
+});
+
+ul.addEventListener('mouseover', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.style.textDecoration = 'highlight';
+    }   
+});
+ul.addEventListener('mouseout', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.style.textDecoration = 'none';
+    }
+});
