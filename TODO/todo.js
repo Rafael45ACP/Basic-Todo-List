81f9e411820bd6066fdd175535fb0db3b0399b3b
Rafael45ACP <rafaelgriego1911@gmail.com>
2026-03-31 22:52:04 +0000

    Implimented checkboxes

--- TODO/todo.js
+++ TODO/todo.js
@@ -6,7 +6,13 @@ function addTask() {
     let task = input.value.trim();
     if (task) {
         let li = document.createElement('li');
-        li.textContent = task;
+        let checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        li.appendChild(checkbox);
+        let span = document.createElement('span');
+        span.textContent = task;
+        li.appendChild(span);
+
         ul.appendChild(li);
         input.value = '';
     }
@@ -25,18 +31,33 @@ input.addEventListener('keydown', function(event) {
 
 
 ul.addEventListener('mouseover', function(event) {
-    if (event.target.tagName === 'LI') {
-        event.target.style.backgroundColor = 'yellow';
-    }   
+    let li = event.target.closest('li');
+    if (li) {
+        li.style.backgroundColor = 'yellow';
+    }
 });
 ul.addEventListener('mouseout', function(event) {
-    if (event.target.tagName === 'LI') {
-        event.target.style.backgroundColor = '';
+    let li = event.target.closest('li');
+    if (li) {
+        li.style.backgroundColor = '';
     }
 });
 
 ul.addEventListener('click', function(event) {
-    if (event.target.tagName === 'LI') {
-        this.removeChild(event.target);
+    if (event.target.tagName === 'SPAN') {
+        event.target.parentElement.remove();
+    }
+});
+
+ul.addEventListener('change', function(event) {
+    if (event.target.type === 'checkbox') {
+        let span = event.target.nextElementSibling;
+        if (event.target.checked) {
+            span.style.textDecoration = 'line-through';
+            span.style.color = 'gray';
+        } else {
+            span.style.textDecoration = '';
+            span.style.color = '';
+        }
     }
 });
